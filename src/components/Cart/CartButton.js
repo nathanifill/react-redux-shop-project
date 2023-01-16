7d5ca58979a5f3708539729ab08f4a2072b1cb6a
Nathan Ifill <nathan.ifill@live.co.uk>
2023-01-16 08:47:01 +0000

    First commit

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,22 @@
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch, useSelector } from "react-redux";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+    // these auto-generated actions are actually action-creator methods that you have to execute and when you execute them, they return action objects. it's the returned action objects which is dispatched here
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
